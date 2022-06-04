d761f674678852867de0c702b80b123a539af803
jjbondoc <101399159+jjbondoc@users.noreply.github.com>
2022-06-04 22:05:08 +0100

    Update main.py

--- hundred-days/main.py
+++ hundred-days/main.py
@@ -1,19 +1,29 @@
-import tkinter
+from tkinter import * # imports every class
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My first GUI Program")
 window.minsize(width=500, height=300)
 
 #* Label
 
-my_label = tkinter.Label(text="I am a Label", font=('Arial', 24, 'bold')) #create
+my_label = Label(text="I am a Label", font=('Arial', 24, 'bold')) #create
 my_label.pack() #use packer to layout the label component
 
+my_label['text'] = "New Text"
+my_label.config(text="New Text")
 
+#* Button
 
+def button_clicked():
+    my_label.config(text=input.get())
+    print("Clicked")
 
+button = Button(text="Click Me", command=button_clicked)
+button.pack()
 
+#* Entry
 
-
+input = Entry(width=10)
+input.pack()
 
 window.mainloop() # keep window on screen
