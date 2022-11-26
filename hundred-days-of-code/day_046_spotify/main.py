297348034d2b3ca4afddd742fa5652b703b3e250
junel <101399159+jjbondoc@users.noreply.github.com>
2022-11-26 19:45:41 +0000

    finish step 1: scrape billboard

--- hundred-days-of-code/day_046_spotify/main.py
+++ hundred-days-of-code/day_046_spotify/main.py
@@ -7,8 +7,10 @@ URL = f"https://www.billboard.com/charts/hot-100/{date}"
 response = requests.get(URL)
 
 soup = BeautifulSoup(response.text, 'html.parser')
-div_list = soup.find(name="div", class_="o-chart-results-list-row-container")
-print(div_list)
+div_list = soup.find_all(name="div", class_="o-chart-results-list-row-container") #obtain each row of the table
+#obtain the first h3 element in the div, get the text, remove \t and \n
+song_list = [div.select_one(selector="ul li ul li h3").getText().strip('\t\n') for div in div_list]
+print(song_list)
 # song_tags = soup.find_all(name="h3", id="title-of-a-story")
 # song_list = [song.getText() for song in song_tags]
 # print(song_list)
