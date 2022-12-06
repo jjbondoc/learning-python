448805531d374b4cc69f993f674ce9b38636d5f5
junel <101399159+jjbondoc@users.noreply.github.com>
2022-12-06 19:47:41 +0000

    complete day 53

--- hundred-days-of-code/day_053_data_entry/main.py
+++ hundred-days-of-code/day_053_data_entry/main.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.service import Service # need for the Service obj
 from selenium.webdriver.common.by import By
 import time
 
-FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLSfxy06jEBb7k_HVVpsr_ZuSF8qaqBn7eN3iBr7n-7Qn8ScUnQ/viewform?usp=sf_link"
+FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLSfxy06jEBb7k_HVVpsr_ZuSF8qaqBn7eN3iBr7n-7Qn8ScUnQ/viewform"
 # ZILLOW_URL = "https://www.zillow.com/homes/for_rent/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22mapBounds%22%3A%7B%22west%22%3A-122.76566542578125%2C%22east%22%3A-122.10099257421875%2C%22south%22%3A37.45193041579769%2C%22north%22%3A38.09724433204761%7D%2C%22mapZoom%22%3A11%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%7D%2C%22isListVisible%22%3Atrue%7D"
 # response = requests.get(url=ZILLOW_URL)
 
@@ -16,10 +16,9 @@ soup = BeautifulSoup(contents, 'lxml')
 
 #TODO: Obtain links
 link_tags = soup.select(selector=".property-card-data a")
-links = ["https://www.zillow.com" + tag.get("href") for tag in link_tags] # get href links
-
-print(len(links))
-print(links)
+links = ["https://www.zillow.com" + tag.get("href") if tag.get("href")[0] == r"/" else tag.get("href") for tag in link_tags ] # get href links
+# print(len(links))
+# print(links)
 
 #TODO: Obtain prices
 price_cards = soup.select(selector=".property-card-data") # get property card div
@@ -33,8 +32,8 @@ for price in prices:
         clean_prices.append(price.split('+')[0])
     else:
         clean_prices.append(price)
-print(len(clean_prices))
-print(clean_prices)
+# print(len(clean_prices))
+# print(clean_prices)
 
 #TODO: Obtain addresses
 address_tags = soup.select(selector=".property-card-link address")
@@ -46,7 +45,40 @@ for address in addresses:
         clean_addresses.append(address.split(" | ")[1])
     else:
         clean_addresses.append(address.split(', ', 1)[1])
-print(len(clean_addresses))
-print(clean_addresses)
+# print(len(clean_addresses))
+# print(clean_addresses)
 
+#TODO: Create a dictionary of properties
+properties = {}
+for i in range(len(links)):
+    properties[i] = {
+        "link": links[i],
+        "price": clean_prices[i],
+        "address": clean_addresses[i]
+    }
 #TODO: Use Selenium to fill in the forms using the lists created
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+options.add_experimental_option('excludeSwitches', ['enable-logging']) # Remove log messages from console
+service = Service(r"C:\Users\josel\Documents\Development\chromedriver.exe")
+driver = webdriver.Chrome(service=service, options=options)
+driver.get(FORM_URL)
+
+for i in range(len(links)):
+    time.sleep(1.5)
+    address_input = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    price_input = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    link_input = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    # input answers
+    address_input.send_keys(f'{properties[i]["address"]}')
+    price_input.send_keys(f'{properties[i]["price"]}')
+    link_input.send_keys(f'{properties[i]["link"]}')
+    # submit response
+    submit = driver.find_element(By.CLASS_NAME, "NPEfkd.RveJvd.snByac")
+    submit.click()
+    # submit another response
+    time.sleep(1.5)
+    submit_another = driver.find_element(By.CSS_SELECTOR, ".c2gzEf a")
+    submit_another.click()
+
+driver.quit()
